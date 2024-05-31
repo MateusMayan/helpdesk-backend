787d6cdd50a30b1e69854cda43b3d873cf598b80
MateusMayan <mateusmayan@icloud.com>
2024-05-31 19:17:29 -0300

    endpoint findById para Tecnico

---
+++ src/main/java/com/mayan/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,28 @@
+package com.mayan.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mayan.helpdesk.domain.Tecnico;
+import com.mayan.helpdesk.services.TecnicoService;
+
+@RestController // Marca uma classe como controlador Spring, ela retorna dados diretamente em vez de exibir uma página HTML. 
+@RequestMapping(value = "/tecnicos") // Mapeia URL's para classes ou métodos específicos.
+public class TecnicoResource {
+
+	@Autowired
+	private TecnicoService service;
+	
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+		Tecnico obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
