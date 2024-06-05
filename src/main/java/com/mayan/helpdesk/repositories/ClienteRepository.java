ee1ea0a6542c154df05561e07f3d94e4f3689c99
MateusMayan <mateusmayan@icloud.com>
2024-06-05 13:53:51 -0300

    CRUD para Cliente incrementado

--- src/main/java/com/mayan/helpdesk/repositories/ClienteRepository.java
+++ src/main/java/com/mayan/helpdesk/repositories/ClienteRepository.java
@@ -2,8 +2,8 @@ package com.mayan.helpdesk.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.mayan.helpdesk.domain.Pessoa;
+import com.mayan.helpdesk.domain.Cliente;
 
-public interface ClienteRepository extends JpaRepository<Pessoa, Integer>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
 }
