10f682546443ef0de1ef3250357091b22e98c1ed
MateusMayan <mateusmayan@icloud.com>
2024-06-10 13:52:54 -0300

    update para Chamado

--- src/main/java/com/mayan/helpdesk/services/ChamadoService.java
+++ src/main/java/com/mayan/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.mayan.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,6 +40,13 @@ public class ChamadoService {
 	public Chamado create(@Valid ChamadoDTO objDTO) {
 		return repository.save(newChamado(objDTO));
 	}
+	
+	 public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+			objDTO.setId(id);
+			Chamado oldObj = findById(id);
+			oldObj = newChamado(objDTO);
+			return repository.save(oldObj);
+		}
 
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
@@ -48,7 +56,11 @@ public class ChamadoService {
 		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
 		}
-
+		
+		if(obj.getStatus().equals(2)) {
+		chamado.setDataFechamento(LocalDate.now());	
+		}
+		
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
@@ -57,4 +69,6 @@ public class ChamadoService {
 		chamado.setObservacoes(obj.getObservacoes());
 		return chamado;
 	}
+
+	
 }
